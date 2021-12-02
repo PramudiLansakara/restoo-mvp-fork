07bb5b8af88d23fb0e7717ffd39185be838f3a23
samaratungajs <it19244044@my.sliit.lk>
2021-12-02 16:20:11 +0530

    fix: Mail route changed

--- api/src/routes/api/mail.route.js
+++ api/src/routes/api/mail.route.js
@@ -6,6 +6,6 @@ const auth = require('../../middlewares/authorization');
 const controller = require('../../controllers/mailServer.controller');
 const validations = require('../../validations/mail.validation');
 
-router.post('/', auth('admin'), validator(validations.sendMail), controller.sendMail);
+router.post('/reservationFeedback', auth('admin'), validator(validations.sendMail), controller.sendMail);
 
 module.exports = router;
