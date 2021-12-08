7a1a2d6d149d9b5ec2a3f05d8ce98ea336971151
samaratungajs <it19244044@my.sliit.lk>
2021-12-08 16:13:28 +0530

    fix: transporter changed

--- api/src/services/transporter.js
+++ api/src/services/transporter.js
@@ -3,37 +3,16 @@ const {
   host, port, username, password,
 } = require('../config').transporter;
 
-// nodemailer.createTestAccount((err, account) => {
-//   const transport = nodemailer.createTransport({
-//     host: 'smtp.ethereal.email',
-//     port: 587,
-//     secure: false, 
-//     auth: {
-//       user: account.user, 
-//       pass: account.pass 
-//     }
-//   });
-// });
-
-
-// const transport = nodemailer.createTransport({
-//   host,
-//   port,
-//   secure: false,
-//   auth: {
-//     user: username,
-//     pass: password,
-//   },
-//   tls:{
-//     rejectUnauthorized: false,
-//   },
-// });
-
 const transport = nodemailer.createTransport({
-  service: 'gmail',
+  host,
+  port,
+  secure: false,
   auth: {
     user: username,
     pass: password,
+  },
+  tls:{
+    rejectUnauthorized: false,
   },
 });
 
