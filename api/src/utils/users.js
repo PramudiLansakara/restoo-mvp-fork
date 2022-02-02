83ea09b51a89700a4d822b03d80b6ea1c41037e4
stharana <stharana2013@gmail.com>
2022-02-03 01:08:38 +0530

    bcrypt->bcryptjs

--- api/src/utils/users.js
+++ api/src/utils/users.js
@@ -1,5 +1,5 @@
 const httpStatus = require('http-status');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const User = require('../models/user.model');
 const APIError = require('./APIError');
 
