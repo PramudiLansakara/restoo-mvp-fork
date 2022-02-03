d70c7ca334c936aa982a33e9d59755ea18b82cac
samaratungajs <it19244044@my.sliit.lk>
2022-02-03 08:07:43 +0530

    fix. footer and drawer img changed

--- client/components/Footer.vue
+++ client/components/Footer.vue
@@ -1,10 +1,25 @@
 <template>
-  <v-footer>
-    <span
-      >Alle Preise inkl. Mwst und Bedienung. Zusatzstoffe und Allergene sind an
-      der Theke einzusehen.</span
+  <v-footer dark
+    padless >
+
+<v-card
+      flat
+      tile
+      class="primary darken-5 white--text text-center"
     >
-    <span>&copy; Mezomia</span>
+      <v-card-text class="white--text pt-1 pb-0">
+        Alle Preise inkl. Mwst und Bedienung. Zusatzstoffe und Allergene sind an
+      der Theke einzusehen.
+      </v-card-text>
+
+      <v-divider></v-divider>
+
+      <v-card-text class="white--text pt-0 pb-0" >
+        {{ new Date().getFullYear() }} — <strong>&copy; Mezomia</strong>
+      </v-card-text>
+    </v-card>
+
+    
   </v-footer>
 </template>
 
