f9dce6d7fb3b28d7f0d7ec48443b0e0e51bdea6d
Pramudi-Lansakara-Spemai <pramudilansakara2001@gmail.com>
2025-09-12 13:07:05 +0530

    Update image path

--- api/src/controllers/imageupload.controller.js
+++ api/src/controllers/imageupload.controller.js
@@ -1,18 +1,38 @@
-const httpStatus = require('http-status');
-const APIError = require('../utils/APIError');
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+const { v4: uuidv4 } = require("uuid");
+const httpStatus = require("http-status");
+const APIError = require("../utils/APIError");
+
+const s3 = new S3Client({ region: process.env.AWS_REGION });
 
 exports.getImagePath = async (req, res, next) => {
   try {
     if (!req.file) {
-      throw new APIError('Image File Not Fount', httpStatus.NOT_FOUND);
+      throw new APIError("Image file not found", httpStatus.NOT_FOUND);
     }
 
-    // const imgPath = (req.file.path)
-    //   .replace(/\\/g, '/')
-    //   .replace('public/', '/');
+    const file = req.file;
+    const fileExtension = file.originalname.split(".").pop();
+    const fileName = `${uuidv4()}.${fileExtension}`;
+    const key = `${process.env.MEDIA_PREFIX}/media/${fileName}`;
 
-    return res.status(httpStatus.OK).json({ imgPath: req.file.key });
+    // Upload file to S3
+    await s3.send(
+      new PutObjectCommand({
+        Bucket: process.env.AWS_S3_BUCKET,
+        Key: key,
+        Body: file.buffer,
+        ContentType: file.mimetype,
+        ACL: "public-read", // allows public access
+      })
+    );
+
+    // Construct the full public URL
+    const imgPath = `https://${process.env.AWS_S3_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com/${key}`;
+
+    return res.status(httpStatus.OK).json({ imgPath });
   } catch (err) {
+    console.error(err);
     next(err);
   }
 };
--- api/src/routes/api/images.route.js
+++ api/src/routes/api/images.route.js
@@ -1,10 +1,10 @@
-const express = require('express');
-
+const express = require("express");
 const router = express.Router();
-const auth = require('../../middlewares/authorization');
-const controller = require('../../controllers/imageupload.controller');
-const fileUpload = require('../../middlewares/upload');
+const auth = require("../../middlewares/authorization");
+const multer = require("multer");
+const controller = require("../../controllers/imageupload.controller");
 
-router.post('/upload', auth('admin'), fileUpload, controller.getImagePath);
+const upload = multer(); // memory storage
+router.post("/upload", auth("admin"), upload.single("file"), controller.getImagePath);
 
 module.exports = router;
